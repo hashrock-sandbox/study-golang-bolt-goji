5a8b335369ba96f2c311dba8ab2a3d927c552e37
hashrock <hashedrock@gmail.com>
2015-09-24 13:02:39 +0900

    simple insert

--- server.go
+++ server.go
@@ -9,53 +9,27 @@ import (
 
 func insert(db *bolt.DB){
     _ = db.Update(func(tx *bolt.Tx) error {
-        bucket, _ := tx.CreateBucketIfNotExists([]byte("MyBucket"))
-        bucket.Put([]byte("1"), []byte("Test"))
+        bucket := tx.Bucket([]byte("MyBucket"))
+        c := bucket.Cursor()
+        k, _ := c.Last()
+        i, _ := strconv.Atoi(string(k[:]))
+        str := fmt.Sprintf("%d", i + 1)
+        bucket.Put([]byte(str), []byte("Test"))
         return nil
     })
 }
 
-/*
-func newPost(db *bolt.DB){
-    db.Update(func(tx *bolt.Tx) error {
-        bucket, err := tx.CreateBucketIfNotExists([]byte("MyBucket"))
-        if err != nil {
-            log.Fatal(err)
-        }    
-        c := bucket.Cursor()
-        k, _ := c.Last()
-        
-        fmt.Printf("last=%s\n", k)
-        fmt.Printf("last=%s\n", len(k))
-
-        num := binary.BigEndian.Uint64(k)
-        fmt.Printf("last=%s\n", num)
-
-        //fmt.Printf("last=%s\n", k + 1)
-        //data := binary.BigEndian.Uint64(k)
-        //fmt.Println(data)
-        err = bucket.Put([]byte(num), []byte("value1"))
-        
-        if err != nil {
-            log.Fatal(err)
-        }
-
-        return nil
-    })    
-}
-*/
-
 func main() {
-    // Open the my.db data file in your current directory.
-    // It will be created if it doesn't exist.
     db, err := bolt.Open("my.db", 0600, nil)
     if err != nil {
         log.Fatal(err)
     }
     
-    //newPost(db)
     insert(db)
-    
+    db.Update(func(tx *bolt.Tx) error {
+        tx.CreateBucketIfNotExists([]byte("MyBucket"))
+        return nil
+    });
     
     db.View(func(tx *bolt.Tx) error {
         b := tx.Bucket([]byte("MyBucket"))
@@ -63,12 +37,6 @@ func main() {
             fmt.Printf("key=%s, value=%s\n", k, v)
             return nil
         })
-        
-        c := b.Cursor()
-        k, _ := c.Last()
-        
-        i, _ := strconv.Atoi(string(k[:]))
-        fmt.Printf("next=%s\n", fmt.Sprint(i + 1))
         return nil
     })
     
